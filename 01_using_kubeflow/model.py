3aaacf714d6d1a9871ddbd1fa8d99e530f9546a5
Vinay.Karumuri <vinaykarumuri15@gmail.com>
2025-04-29 17:01:59 -0500

    changing buk

--- 01_using_kubeflow/model.py
+++ 01_using_kubeflow/model.py
@@ -30,7 +30,7 @@ def build_model(aws_access_key_id: str, aws_secret_access_key: str):
     from sklearn.linear_model import LinearRegression
     # from sklearn.metrics import mean_squared_error
 
-    bucket = "vinaykubeflow"
+    bucket = "vinaykubelow"
     data_s3_path = "data/rental_1000.csv"
     model_s3_path = "model/rental_prediction_model.pkl"
     local_data_path = "rental_1000.csv"
